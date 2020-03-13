17a08f9c505de863b7c832bb8fb2bf725e292a17
ivan-herceg <48533219+ivan-herceg@users.noreply.github.com>
2020-03-13 21:48:47 +0100

    Add files via upload

---
+++ build/precache-manifest.113d32a6baaf3892d0ca0593a5cf42e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4129ed42769b0fdebfa4d7afe8bd6ae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23bc86fe75da55729598",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ce55d20d7886f18688da",
+    "url": "/static/js/2.c3aed42a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c3aed42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23bc86fe75da55729598",
+    "url": "/static/js/main.27180e3f.chunk.js"
+  },
+  {
+    "revision": "95b00f7e967302e9ee68",
+    "url": "/static/js/runtime-main.7873e549.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
